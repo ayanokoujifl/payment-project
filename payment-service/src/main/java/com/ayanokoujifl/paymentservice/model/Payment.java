e0d81e7c07ac5b4f63ebc341b4506111ceef3383
ayanokoujifl <guleite3@gmail.com>
2025-06-03 17:23:42 -0300

    Initialized project

---
+++ payment-service/src/main/java/com/ayanokoujifl/paymentservice/model/Payment.java
@@ -0,0 +1,44 @@
+package com.ayanokoujifl.paymentservice.model;
+
+import java.io.Serializable;
+
+public class Payment implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private Long userId;
+	private Long productId;
+	private String cardNumber;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+}
